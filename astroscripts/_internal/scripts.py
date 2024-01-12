913e8b46255ff72386453e6878df4d4c1c596dc6
katapin <atapin.kirill@gmail.com>
2024-01-12 19:09:07 +0300

    FIX: venv activation, auxil init scripts

--- astroscripts/_internal/scripts.py
+++ astroscripts/_internal/scripts.py
@@ -1,4 +1,4 @@
-"""Provide executables for the package."""
+"""Provide executables for the package (described in pyproject.toml's [project.scripts] section)."""
 
 import os
 import sys
@@ -15,30 +15,40 @@ AUXILINIT = {                  # additional init command for the specific collec
     'chandra': 'ciaoinit',
 }
 
+def _get_activate_path():
+    """Return path to the venv's activate script."""
+    bindir = os.path.dirname(sys.executable)  # Dir with python interpreter
+    actpath = bindir + '/activate'
+    return actpath if os.path.exists(actpath) else None
+
 
 def init():
+    """Active venv and set $PATH variable.
+
+    This functions starts a child bash process with activated python
+    virtual environment and modified $PATH variable. To do this, it
+    creates own temporary init bash script.
+    """
     parser = argparse.ArgumentParser(
-        description="Initialize collections of scripts")
+        description="Initialize specific collection of scripts")
     parser.add_argument(
         'name', nargs='*', help="name of collection")
     parser.add_argument(
         '-v', '--version', action='store_true', help="Print package version")
+    parser.add_argument(
+        '-x', '--auxil', action='store_true', help="Additionally call the collection "
+        "specific init script.")
     argnspace = parser.parse_args(sys.argv[1:])
 
-    initlines = [
-        'source ~/.bashrc\n',
-        'PS1="\033[1;34m${PS1}\033[0m"\n'
-        'heainit\n'
-    ]
-
     pkgname = getmainname(__package__)   # Name of the main package
-    pathlines = []
+    pathlines = []  # Store paths to the claimed collections to append to $PATH
 
     # Print version info
     if argnspace.version:
         print('{}, version {}'.format(pkgname, importlib.metadata.version(pkgname)))
         exit(0)
 
+    # Probe the folder that we intend to make accessible
     try:
         rootpath = os.path.dirname(__file__) + '/' + SCRIPTROOTDIR
         allnames = os.listdir(rootpath)
@@ -46,20 +56,32 @@ def init():
         die("Can't find folder with scripts. The package might be corrupted.", progname=pkgname)
 
     # Process 'all' option
-    if len(argnspace.name) == 0 or 'all' in argnspace.name:
+    if argnspace.name == 'all' in argnspace.name:
         names = allnames
     else:
         names = argnspace.name
+
+    initshlines = [             # Content of the init script
+        'source ~/.bashrc\n',
+        '_PS1_bak=${PS1}\n']
+    if actpath := _get_activate_path():
+        initshlines.append(f'source {actpath}\n')  # add 'activate' scripts if it exists
+    initshlines += [
+        'PS1="\033[1;34m${_PS1_bak}\033[0m"\n',
+        'heainit\n'  # Init HEASOFT
+    ]
 
     for folder in names:
         if folder not in allnames:
             die(f"Script collection '{folder}' is not found.", progname=pkgname)
         pathlines.append(rootpath + '/' + folder)
-        initlines.append(AUXILINIT.get(folder,'')+'\n')
+        if argnspace.auxil:  # Add collection-specific init script
+            initshlines.append(AUXILINIT.get(folder, '')+'\n')
+
 
     with NamedTemporaryFile(mode='a') as initsh:
-        initlines.append('export PATH={}:$PATH\n'.format(
+        initshlines.append('export PATH={}:$PATH\n'.format(
             ':'.join(pathlines)))
-        initsh.writelines(initlines)
+        initsh.writelines(initshlines)
         initsh.flush()
         os.system(f'bash --init-file {initsh.name}')
