6563b11f96005a55b3a5c716d4caac500673b970
katapin <atapin.kirill@gmail.com>
2024-02-12 20:29:03 +0300

    ColumnBased finished

--- astroscripts/_internal/_columnbased.py
+++ astroscripts/_internal/_columnbased.py
@@ -1,10 +1,14 @@
 
+from copy import deepcopy
+from abc import ABC, abstractmethod
+from itertools import chain
+from collections.abc import KeysView
+from functools import singledispatchmethod
+from typing import Iterable, Sized, Self
+# from mypythonlib.collections import ROdict
+
 import numpy as np
 from numpy.typing import NDArray, ArrayLike
-from typing import Iterable, Sized
-from abc import ABC, abstractmethod
-from functools import singledispatchmethod
-from mypythonlib.collections import ROdict
 
 
 def _make_vector(vec: ArrayLike | None, vecname: str = None, *,
@@ -28,85 +32,215 @@ def _make_vector(vec: ArrayLike | None, vecname: str = None, *,
         if not isinstance(vec, Sized):   # numpy can't create arrays from iterator
             vec2 = tuple(vec)
 
-        if len(vec2) == 0:
-            raise ValueError(
-                "The vector {}{}cannot be empty.".format(
-                    _vecname,
-                    'can be None but ' if none_is_allowed else ''
-                )
-            )
+        #C an the vector by empty?
+        # if len(vec2) == 0:
+        #     raise ValueError(
+        #         "The vector {}{}cannot be empty.".format(
+        #             _vecname,
+        #             'can be None but ' if none_is_allowed else ''
+        #         )
+        #     )
 
         res = np.array(vec2)  # create the array to return
 
         if (res.ndim > 1 or    # the original vec was multidimensional (nested)
                 (reflen and reflen != len(res))):   # different length
-            raise ValueError("All the arrays must be 1D of the the same length.")
+            raise ValueError("All the arrays must be 1D of the the same length but "
+                             f"{_vecname}doesn't.")
         if res.ndim == 1:
             return res
 
-    else:  # probably, this is a number
-        if from_numbers and reflen:
-            try:
-                flt = float(vec)   # test for a number
-                res = np.ones((1, reflen)) * flt  # Create 1D array
-                return res
-            except Exception:
-                raise TypeError(_typertext + ' or a single number.')
+    elif from_numbers:  # probably, this is a number
+        if not isinstance(reflen, int):
+            raise TypeError(f"Integer 'reflen' is expected but {reflen} is received.")
+        try:
+            flt = float(vec)   # test for a number
+            res = np.ones((1, reflen)) * flt  # Create 1D array
+            return res
+        except Exception:
+            raise TypeError(_typertext + ' or a single number.')
 
     # vec was a string or something not digestible by numpy
-    raise TypeError(_typertext + '.')
+    raise TypeError(_typertext + f' (not {type(vec)}).')
 
 
-class ColumnStorage(ROdict):
-    """Read-only dict to store ndarray vectors."""
+# class ColumnStorage(ROdict):
+#     """Read-only dict to store ndarray vectors."""
+#
+#     def _item_change_existing_as_normal(self, key, val):
+#         self._dict[key][:] = val
+#
+#     def _item_get_missing_as_normal(self, key):
+#         raise KeyError(f"Column '{key}' doesn't exist.")
+#
+#     def _item_del_missing_as_normal(self, key):
+#         raise KeyError(f"Column '{key}' doesn't exist.")
+#
+#     def __str__(self):
+#         """Return string representation."""
+#         text = self.__class__.__name__ + ': {\n'
+#         lst = []
+#         for k, v in self._dict.items():
+#             lst.append((f"'{k}'*:" if k in self._readonly else f"'{k}':") + \
+#                        repr(v))
+#         return text + '\n'.join(lst) + ' }'
 
-    def _item_change_existing_as_normal(self, key, val):
-        self._dict[key][:] = val
 
-    def _item_get_missing_as_normal(self, key):
-        raise KeyError(f"Column '{key}' doesn't exist.")
+# class _ColDesc():
+#     """Descriptor"""
+#
+#
+#
+#     def __set_name__(self, owner, name):
+#         self.name = name
+#
+#     def __get__(self, obj, objtype=None):
+#         print(obj)
+#         if not obj:
+#             return
+#         if obj._exposecols is False:
+#             raise AttributeError(f"There is no attribute 'self.name'")
+#
+#         return self.Proxy(obj)
 
-    def _item_del_missing_as_normal(self, key):
-        raise KeyError(f"Column '{key}' doesn't exist.")
 
-    def __str__(self):
-        """Return string representation."""
-        text = self.__class__.__name__ + ': {\n'
-        lst = []
-        for k, v in self._dict.items():
-            lst.append((f"'{k}'*:" if k in self._readonly else f"'{k}':") + \
-                       repr(v))
-        return text + '\n'.join(lst) + ' }'
-
-
-class ColumnBased(ABC):
+class _ColumnBasedMinimal(ABC):
     """Abstract class for column-based objects like light curves,
-     spectra, plots, etc. Provide sampling via masks and
-     slices. Assumes that the data is stored in _columns and
-     _meta attributes."""
+    spectra, plots, etc. Provide sampling via masks and
+    slices. It assumes that all the passed columns are
+    valid ndarrays of the same length (it doesn't check this).
+    """
 
     @abstractmethod
-    def __init__(self, columns: ColumnStorage, meta: ROdict):
-        self._columns = columns
-        self._meta = meta
+    def __init__(self, columns: dict[str, NDArray] | None = None):
+        self._columns = columns.copy() if columns else {}
+
+    def __len__(self):
+        """Return length of the columns."""
+        if len(self._columns):
+            fk = tuple(self._columns.keys())[0]  # first key
+            return len(self._columns[fk])
+        else:  # There are no columns
+            return 0
+
+    def __eq__(self, other: Self):
+        if type(self) is not type(other):
+            return NotImplemented
+        return (
+            self._columns.keys() == other._columns.keys() and        # test keys
+            all(np.all(np.isclose(self._columns[name], other._columns[name]))  # and test each column
+                for name in self._columns)
+        )
 
     @singledispatchmethod
-    def __getitem__(self, key):
-        raise TypeError("Unsupported type of key: {}.".format(type(key)))
+    def __getitem__(self, arg):
+        raise TypeError("Unsupported type of key: {}.".format(type(arg)))
+
+    @__getitem__.register(int)
+    def _(self, arg: int):
+        return self._getitem_point(arg)
 
     @__getitem__.register
-    def _(self, index: int):
-        """Must be implemented by the subclass."""
-        return NotImplemented
+    def _(self, arg: slice | list | np.ndarray):
+        return self._getitem_subsample(arg)
 
-    @__getitem__.register
-    def __getitem__(self, arg: slice, list, ndarray):
-        obj = object.__new__(self.__class__)
-        obj._meta = self._meta
-        # for col in self._columns
-        for vecname in ['main', 'lower', 'upper']:
-            obj._values[vecname] = obj._values[vecname][arg]
+    def _getitem_point(self, index: int) -> dict[str, float]:
+        """Get a single point (in multidimensional space)."""
+        return {n: float(ar[index]) for n, ar in self._columns.items()}
+
+    def _getitem_subsample(self, arg: slice | ArrayLike) -> Self:
+        """Get a subsample via slice or mask."""
+        obj = self.copy()
+        for colname in obj._columns:
+            obj._columns[colname] = obj._columns[colname][arg]
+        if hasattr(obj, '_rebuild_hook'):  # The object may require additional tuning
+            obj._rebuild_hook()
         return obj
 
-    import copy
+    def copy(self) -> Self:
+        """Return a deep copy of the object."""
+        # It's fantastic but self.__proxy.owner = self is treated correctly!
+        return deepcopy(self)
 
+class _ColumnBased(_ColumnBasedMinimal):
+    """Extends ColumnBasedMinimal. Provide access to columns
+    through dynamic attributes of the same name and through
+    dedicated self.columns object. The latter method returns
+    a read-only view of the ndarray for the protected columns
+    and a normal view for the remaining. The attribute access
+    always returns a read-only view.
+    """
+
+    class __ColumnProxy:
+        """The proxy object to be returned via self.columns,
+        provides dict-like access to the columns."""
+        def __init__(self, owner):
+            self.owner = owner
+
+        def __contains__(self, key: str) -> bool:
+            return key in self.owner._columns
+
+        def __get_column_raw(self, key: str) -> NDArray:
+            if key not in self.owner._columns:
+                raise KeyError(f"Column '{key}' does not exist")
+            return self.owner._columns[key]
+
+        def __getitem__(self, key: str) -> NDArray:
+            view = self.__get_column_raw(key).view()
+            if key in self.owner._pcolnames:        # If protected
+                view.flags.writeable = False        # make the view read-only
+            return view
+
+        def __setitem__(self, key: str, value: NDArray):
+            if key in self.owner._pcolnames:
+                # We use ValueError instead of TypeError to match
+                # the numpy behaviour when writing to the writeable=False
+                # destination array
+                raise ValueError(f"assignment ('{key}') destination is read-only")
+            self.__get_column_raw(key)[:] = value   # numpy broadcasting guaranties the same shape
+
+    @abstractmethod
+    def __init__(self, columns: dict[str, NDArray] | None = None,
+                 pcolumns: dict[str, NDArray] | None = None):
+        """
+        :param columns: dict of ordinary columns
+        :param pcolumns: dict of protected columns
+        """
+        super().__init__(columns)
+        self._pcolnames = set()        # Names of the protected columns
+        if pcolumns:
+            self._columns.update(pcolumns)
+            self._pcolnames.update(pcolumns.keys())
+
+        self.__proxy = self.__ColumnProxy(self)    # Dict-like access
+
+    def __eq__(self, other: Self):
+        return (super().__eq__(other) and
+            self._pcolnames == other._pcolnames)
+
+    def __dir__(self):
+        """Provide autocomplete for dynamic attributes in ipython."""
+        return chain(object.__dir__(self), self._columns.keys())
+
+    def __getattr__(self, item):
+        try:
+            col = self.columns[item]
+            # Make every column read-only independently on its protection status
+            col.flags.writeable = False
+        except KeyError:
+            raise AttributeError(f"There is no attribute '{item}'")
+        return col
+
+    @property
+    def colnames(self):
+        """Return names of all the columns."""
+        return self._columns.keys()
+
+    @property
+    def pcolnames(self):
+        """Return names of the protected columns."""
+        return KeysView(self._pcolnames)
+
+    @property
+    def columns(self):
+        return self.__proxy
---
+++ tests/test_columnbased.py
@@ -0,0 +1,287 @@
+import pytest
+import numpy as np
+from numpy import array
+from astroscripts._internal._columnbased import (
+    _make_vector,
+    _ColumnBased,
+    _ColumnBasedMinimal,
+)
+
+
+class Test_make_vector():
+
+    @pytest.mark.parametrize(
+        "input,expected,noneallowed,from_numbers,reflen",
+        [
+            ([1, 2, 3], array([1, 2, 3]), False, False, None),
+            ([1, 2, 3], array([1, 2, 3]), True, False, None),
+            ([1, 2, 3], array([1, 2, 3]), False, True, None),
+            ([1, 2, 3], array([1, 2, 3]), False, True, 3),
+            ((1, 2, 3), array([1, 2, 3]), False, False, None),
+            (array([1, 2, 3]), array([1, 2, 3]), False, False, None),
+            (iter([1, 2, 3]), array([1, 2, 3]), None, False, None),
+            (range(5), array([0, 1, 2, 3, 4]), None, False, None),
+            (None, None, True, False, None),
+            (2, array([2, 2, 2]), False, True, 3),
+        ]
+    )
+    def test_valid_input(self, input, expected, noneallowed, from_numbers, reflen):
+        output = _make_vector(input, none_is_allowed=noneallowed,
+                              from_numbers=from_numbers, reflen=reflen)
+        print(type(output))
+        assert type(output) == type(expected)
+        assert np.all(output == expected)
+
+    @pytest.mark.parametrize(
+        "input, noneallowed, from_numbers, excls, reflen",
+        [
+            (None, False, False, TypeError, None),
+            (None, False, True, TypeError, None),
+            ('qwe', True, False, TypeError, None),
+            ('qwe', True, True, TypeError, None),
+            # ([], False, False, ValueError, None),     # cannot be empty
+            # ([], True, False, ValueError, None),
+            ([1,2,3], False, False, ValueError, 4),   # wrong length
+            (array([[1,2,3],[4,5,6]]), False, False, ValueError, None),  # 2D is not allowed
+            ([[1,2,3],[4,5,6]], False, False, ValueError, None),         # as well as nested lists
+            (5, False, True, TypeError, None),        # from numbers but reflen is not passed
+        ]
+    )
+    def test_invalid_input(self, input, noneallowed, from_numbers, excls, reflen):
+        with pytest.raises(excls) as exception:
+            _make_vector(input, vecname='values', none_is_allowed=noneallowed,
+                         from_numbers=from_numbers, reflen=reflen)
+
+
+def mkcols(names: list[str], data):
+    """Prepare columns for tests."""
+    return {n: (_make_vector(a if not isinstance(a, int) else range(a)))
+            for n,a in zip(names, data)}
+
+
+class Test_ColumnBasedMinimal():
+    _ColumnBasedMinimal.__abstractmethods__ = set()
+
+    @pytest.mark.parametrize(
+        "cols, res",
+        [
+            (mkcols(['rate'], [5]), 5),
+            (None, 0)  # Empty object
+        ]
+    )
+    def test_len(self, cols, res):
+        cbm = _ColumnBasedMinimal(cols)
+        assert len(cbm) == res
+
+    @pytest.mark.parametrize(
+        "cols, index, res",
+        [
+            (mkcols(['X', 'Y'], [5, 5]), 2, {'X': 2, 'Y': 2}),
+        ]
+    )
+    def test_getitem_point(self, cols, index, res):
+        cbm = _ColumnBasedMinimal(cols)
+        assert cbm[index] == res
+
+    @pytest.mark.parametrize(
+        "cols, mask, res",
+        [
+            (  # boolean mask
+                mkcols(['X', 'Y'], [5, 5]),
+                [False, True, True, False, False],
+                mkcols(['X', 'Y'], [[1, 2], [1, 2]])
+            ),
+            (  # indexing array
+                    mkcols(['X', 'Y'], [5, 5]),
+                    [1, 2],
+                    mkcols(['X', 'Y'], [[1, 2], [1, 2]])
+            ),
+            (  # indexing array
+                    mkcols(['X', 'Y'], [5, 5]),
+                    slice(1, 3),
+                    mkcols(['X', 'Y'], [[1, 2], [1, 2]])
+            ),
+        ]
+    )
+    def test_getitem_subsample(self, cols, mask, res):
+        cbm = _ColumnBasedMinimal(cols)
+        cmb2 = _ColumnBasedMinimal(res)
+        new = cbm[mask]
+        assert all(list(new._columns['X']) == res['X'])
+        assert new == cmb2
+
+    def test_copy(self):
+        # TODO
+        pass
+
+
+
+class Test_ColumnBased():
+    _ColumnBased.__abstractmethods__ = set()
+    @pytest.mark.parametrize(
+        "cols, pcols",
+        [
+            (mkcols(['rate'], [5]), None),    # no protected
+            (None, mkcols(['time'], [5])),    # no normal
+            (mkcols(['rate'], [5]), mkcols(['time'], [5])),    # expose
+        ]
+    )
+    def test_init(self, cols, pcols):
+        cb = _ColumnBased(cols, pcols)
+        cols, pcols = cols or {}, pcols or {}
+        allcols = list(cols.keys()) + list(pcols.keys())
+        assert list(cb._columns) == allcols   # Normal and protected are stored together
+        assert list(cb.pcolnames) == list(pcols.keys())
+
+    @pytest.mark.parametrize(
+        "cols, pcols, cols2, pcols2, res",
+        [
+            (
+                mkcols(['rate'], [5]),
+                None,
+                mkcols(['rate'], [5]),
+                None,
+                True
+            ), # OK
+            (
+                mkcols(['rate'], [5]),
+                None,
+                mkcols(['rate'],[[1, 1, 1, 1, 1]]),
+                None,
+                False
+            ),  # Different vectors
+            (
+                mkcols(['rate'], [5]),
+                None,
+                mkcols(['rate2'], [5]),
+                None,
+                False
+            ),  # Different names
+            (
+                    mkcols(['rate'], [5]),
+                    mkcols(['time'], [5]),
+                    mkcols(['rate'], [5]),
+                    None,
+                    False
+            ),  # Different protected columns
+            (
+                    mkcols(['rate'], [5]),
+                    mkcols(['time'], [5]),
+                    mkcols(['rate'], [5]),
+                    mkcols(['time'], [5]),
+                    True
+            ),  # OK
+            (
+                    mkcols(['rate', 'time'], [5, 5]),
+                    None,
+                    mkcols(['rate'], [5]),
+                    mkcols(['time'], [5]),
+                    False
+            ),  # Names are the same but not protected
+        ]
+    )
+    def test_eq(self, cols, pcols, cols2, pcols2, res):
+        cb = _ColumnBased(cols, pcols)
+        cb2 = _ColumnBased(cols2, pcols2)
+        assert (cb == cb2) is res
+
+    @pytest.mark.parametrize(
+        "cols, pcols",
+        [
+            (mkcols(['rate'], [5]), None),    # no protected
+            (None, mkcols(['time'], [5])),    # no normal
+            (mkcols(['rate'], [5]), mkcols(['time'], [5])),    # expose
+        ]
+    )
+    def test_getattr(self, cols, pcols):
+        """Check whether columns accessible as dynamic attributes."""
+        cb = _ColumnBased(cols, pcols)
+        for coldict in (cols, pcols):
+            if coldict:
+                colname = list(coldict.keys())[0]
+                assert hasattr(cb, colname)
+
+                colvals = getattr(cb, colname)
+                assert np.all(colvals == coldict[colname])
+                assert colvals is not coldict[colname]
+
+    @pytest.mark.parametrize(
+        "cols, pcols",
+        [
+            (mkcols(['rate'], [5]), None),    # no protected
+            (None, mkcols(['time'], [5])),    # no normal
+            (mkcols(['rate'], [5]), mkcols(['time'], [5])),    # expose
+        ]
+    )
+    def test_columns_get(self, cols, pcols):
+        """Check whether columns accessible as dynamic attributes."""
+        cb = _ColumnBased(cols, pcols)
+        for coldict in (cols, pcols):
+            if coldict:
+                colname = list(coldict.keys())[0]
+                assert (colname in cb.columns)
+
+                colvals = cb.columns[colname]
+                assert np.all(colvals == coldict[colname])
+                assert (colvals is not coldict[colname])
+
+
+    @pytest.mark.parametrize(
+        "cols, pcols, excls",
+        [
+            (mkcols(['rate'], [5]), None, None),          # no protected
+            (None, mkcols(['time'], [5]), ValueError),    # no normal
+        ]
+    )
+    def test_columns_modify(self, cols, pcols, excls):
+        """Check whether columns accessible as dynamic attributes."""
+        cb = _ColumnBased(cols, pcols)
+        for coldict in (cols, pcols):
+            if coldict:
+                colname = list(coldict.keys())[0]
+                if colname in cb.pcolnames:  # The column is protected
+                    with pytest.raises(excls) as exception:
+                        cb.columns[colname][0] = 55
+                else:  # The column is normal
+                    cb.columns[colname][0] = 55
+                    res = coldict[colname].copy()  # Modify initial input value
+                    res[0] = 55
+                    assert np.all(cb.columns[colname] == res)
+
+
+    @pytest.mark.parametrize(
+        "cols, pcols, new, excls",
+        [
+            (
+                mkcols(['rate'], [5]),
+                None,
+                array([1, 1, 1, 1, 1]),
+                None
+            ),  # everything is OK
+            (
+                mkcols(['rate'], [5]),
+                None,
+                array([1, 1, 1, 1]),
+                ValueError
+            ),  # wrong length
+            (
+                None,
+                mkcols(['time'], [5]),
+                array([1, 1, 1, 1, 1]),
+                ValueError
+            ),  # trying to modify protected
+        ]
+    )
+    def test_columns_set(self, cols, pcols, new, excls):
+        """Check whether columns accessible as dynamic attributes."""
+        cb = _ColumnBased(cols, pcols)
+        for coldict in (cols, pcols):
+            if coldict:
+                colname = list(coldict.keys())[0]
+                if excls:  # We're expecting an error
+                    with pytest.raises(excls) as exception:
+                        cb.columns[colname] = new
+                else:
+                    cb.columns[colname] = new
+                    assert np.all(cb.columns[colname] == new)
+
